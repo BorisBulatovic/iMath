df4db4886a68216ab48e746bac2f6a74806ad1e6
BorisBulatovic <bbulatovic@isb.edu.rs>
2014-04-28 09:06:48 +0200

    Create header.inc.php

---
+++ header.inc.php
@@ -0,0 +1,161 @@
+<?php include ("./inc/connect.inc.php") ?>
+
+<html class=" js flexbox flexboxlegacy canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths"><head><script type="text/javascript" async="" src="http://tracker.marinsm.com/tracker/async/2932in917575.js"></script><script type="text/javascript" charset="utf-8" async="" data-requirecontext="_" data-requiremodule="jquery" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script><script type="text/javascript" charset="utf-8" async="" data-requirecontext="_" data-requiremodule="/s/resources/en-US/webs.external.index.signup/?callback=define" src="/s/resources/en-US/webs.external.index.signup/?callback=define"></script></head><body class="page-signup footer-slim signup signup-themepicker themepicker">html prefix="og: http://ogp.me/ns#" class=" js flexbox flexboxlegacy canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths"&gt;<!--<![endif]--><script async="" src="//www.googletagmanager.com/gtm.js?id=GTM-N4XF&amp;l=_gtmTrack"></script><script type="text/javascript" async="" src="http://api.mixpanel.com/site_media/js/api/mixpanel.2.js"></script><script type="text/javascript" async="" src="//cdn3.optimizely.com/js/geo.js"></script>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=1">
+<title>Sign Up for our Free Website Builder Now  | Webs</title>
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+<link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="http://static.websimages.com/v7296ec2/active-static/target/external/css/base.css">
+
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+
+<script type="text/javascript" async="" src="http://stats.g.doubleclick.net/dc.js"></script><script type="text/javascript" async="" src="http://tracker.marinsm.com/tracker/async/2932in917575.js"></script><script async="" src="//www.googletagmanager.com/gtm.js?id=GTM-N4XF&amp;l=_gtmTrack"></script><script type="text/javascript" async="" src="http://api.mixpanel.com/site_media/js/api/mixpanel.2.js"></script><script type="text/javascript" src="http://members.webs.com/JS/checkLogin.jsp?r=0.8158430490682522&amp;fromSignup=1&amp;ss=null"></script><style type="text/css"></style><style type="text/css"></style>
+
+<script>var websData = { prefetchSbAssets: true, globalAssetBaseURL: 'http://static.websimages.com/v7296ec2', category: '241', subcategory: '' };</script>
+<meta name="decorator" content="external">
+<meta name="description" content="Sign up to create a free website from Webs. Quick, easy, and ideal for small businesses and ecommerce.">
+
+<script>
+var beforeload = (new Date()).getTime();
+</script>
+
+<link rel="stylesheet" href="http://static.websimages.com/v7296ec2/active-static/target/external/css/signup.css">
+
+<script src="http://static.websimages.com/v7296ec2/JS/Tracker.js"></script>
+
+<script type="text/javascript">
+(function(d, c) {
+var a,b,g,e;
+a = d.createElement("script");
+a.type = "text/javascript";
+a.async = !0;
+a.src = ("https:" === d.location.protocol ? "https:" : "http:") +
+'//api.mixpanel.com/site_media/js/api/mixpanel.2.js';
+b = d.getElementsByTagName("script")[0];
+b.parentNode.insertBefore(a, b);
+c._i = [];
+c.init = function(a, d, f) {
+var b = c;
+"undefined" !== typeof f ? b = c[f] = [] : f = "mixpanel";
+g = ['disable','track','track_pageview',
+'track_links','track_forms','register','register_once','unregister','identify',
+'name_tag','set_config'];
+for (e = 0; e < g.length; e++)(function(a) {
+b[a] = function() {
+b.push([a].concat(
+Array.prototype.slice.call(arguments, 0)))
+}
+})(g[e]);
+c._i.push([a,d,f])
+};
+window.mixpanel = c
+}
+)(document, []);
+mixpanel.init("58fa82747b4c3f4992b74583e70b8940");
+</script>
+
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-33130564-1']);
+_gaq.push(['_setDomainName', '.webs.com']);
+_gaq.push(['_setAllowHash', false]);
+_gaq.push(['_setAllowLinker', true]);
+_gaq.push(['_trackPageview']);
+</script>
+
+<script type="text/javascript">
+
+function getCookie(cookieName) {
+var cookies = document.cookie.split(";");
+var name, value;
+for (var i = 0; i < cookies.length; i++) {
+name = cookies[i].substr(0, cookies[i].indexOf("=")).trim();
+if (name == cookieName) {
+return cookies[i].substr(cookies[i].indexOf("=") + 1).trim();
+}
+}
+return;
+}
+
+_gaq.pop();
+_gaq.push(['_trackPageview', '/Signup/']);
+
+
+Tracker.init("NewAcct");
+Tracker.track("newSignupCreation");
+</script>
+<script src="http://static.websimages.com/v7296ec2/active-static/lib/modernizr.js"></script>
+<script>
+var webs = {};
+webs.locale = '';
+
+webs.props = {
+globalAssetBaseURL: 'http://static.websimages.com/v7296ec2'
+}
+
+           webs.themeFirstSignupEnabled = false;
+       </script>
+
+<script type="text/javascript" charset="utf-8" async="" data-requirecontext="_" data-requiremodule="external/js/common" src="http://static.websimages.com/v7296ec2/active-static/target/external/js/common.js"></script><script type="text/javascript" charset="utf-8" async="" data-requirecontext="_" data-requiremodule="external/js/signup" src="http://static.websimages.com/v7296ec2/active-static/target/external/js/signup.js"></script><script type="text/javascript" charset="utf-8" async="" data-requirecontext="_" data-requiremodule="dust-helpers" src="http://static.websimages.com/v7296ec2/active-static/target/dust-helpers.js"></script><script type="text/javascript" charset="utf-8" async="" data-requirecontext="_" data-requiremodule="dust-core" src="http://static.websimages.com/v7296ec2/active-static/target/../lib/dust/dust-core.js"></script><link rel="stylesheet" type="text/css" href="http://static.websimages.com/v7296ec2/active-static/target/ui/themepicker.css"><script type="text/javascript" charset="utf-8" async="" data-requirecontext="_" data-requiremodule="/s/resources/en-US/webs.external.index.signup/?callback=define" src="/s/resources/en-US/webs.external.index.signup/?callback=define"></script><script type="text/javascript" charset="utf-8" async="" data-requirecontext="_" data-requiremodule="/s/resources/en-US/webs.themepicker/?callback=define" src="/s/resources/en-US/webs.themepicker/?callback=define"></script><script type="text/javascript" charset="utf-8" async="" data-requirecontext="_" data-requiremodule="/s/resources/en-US/webs.util/?callback=define" src="/s/resources/en-US/webs.util/?callback=define"></script><script type="text/javascript" charset="utf-8" async="" data-requirecontext="_" data-requiremodule="/s/resources/en-US/webs.external.signup.themepicker/?callback=define" src="/s/resources/en-US/webs.external.signup.themepicker/?callback=define"></script><script type="text/javascript" charset="utf-8" async="" data-requirecontext="_" data-requiremodule="webs/props" src="http://static.websimages.com/v7296ec2/active-static/target/webs/props.js"></script>
+
+<div id="header-wrapper">
+<header id="webs-header">
+<div class="section-container clearfix">
+<a class="logo" href="/" title="Webs">Webs</a>
+
+<div id="webs-header-content" style="height: auto;">
+<nav id="webs-nav">
+<ul>
+<li class="howitworks">
+<a href="/how-it-works">
+How It Works</a>
+</li>
+<li class="gallery">
+<a href="/themes">
+Themes</a>
+</li>
+<li class="features">
+<a href="/features">
+Features</a>
+</li>
+<li class="whywebs">
+<a href="/why-webs">
+Why Webs</a>
+</li>
+<li class="plans">
+<a href="/plans">
+Plans</a>
+</li>
+</ul>
+</nav>
+<div class="actions">
+<div id="sign-in">
+<div class="dropdown">
+<form method="POST" action="http://members.webs.com/j_spring_security_check" name="login_leaf" class="login-leaf emphasis">
+<div class="form-error login-error">Please enter a valid email address and password</div>
+<input id="FWloginUsername" type="email" class="FWloginUsername" spellcheck="false" name="j_username" placeholder="Email Address">
+<input id="FWloginPassword" type="password" class="FWloginPassword" spellcheck="false" name="j_password" placeholder="Password">
+<input type="submit" alt="Sign In" title="Sign In" class="webs-ext-btn webs-ext-btn-blue submit" value="Sign In">
+</form>
+<a href="/s/login/forgotPassword" class="forgot-password" title="Forgot Password?">Forgot Password?</a>
+</div>
+<a href="/s/login/relogin/" class="webs-ext-btn webs-ext-btn-grey" title="Sign In">
+<span>
+Sign In</span>
+</a>
+</div>
+<a href="/Signup" title="Sign Up Free" class="webs-ext-btn webs-ext-btn-green sign-up">
+<span>
+Sign Up Free</span>
+</a>
+</div>
+</div>
+
+<div id="webs-mobile-header-select"></div>
+</div>
+</header>
+</div>
