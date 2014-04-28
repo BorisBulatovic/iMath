6bca8cceaa141aead7ca6a5f10f687c63d4c7fac
BorisBulatovic <bbulatovic@isb.edu.rs>
2014-04-28 09:11:24 +0200

    Create footer.inc.php

---
+++ footer.inc.php
@@ -0,0 +1,57 @@
+<footer id="webs-footer">
+<div class="section-container clearfix">
+<div class="footer-right">
+<a href="/Signup" id="start-button" class="webs-ext-btn webs-ext-btn-green webs-ext-btn-arrow" title="Create a free website">
+<span>
+Create a free website</span>
+<i class="arrow"></i>
+</a>
+<div class="social">
+<a href="https://plus.google.com/100830269646287541736" target="_blank" class="google webs-ext-btn" title="Google Plus">
+<span class="icon">Google Plus</span>
+</a>
+<a href="http://www.facebook.com/#!/Webs" target="_blank" class="facebook webs-ext-btn" title="Facebook">
+<span class="icon">Facebook</span>
+</a>
+<a href="http://twitter.com/webs" target="_blank" class="twitter webs-ext-btn" title="Twitter">
+<span class="icon">Twitter</span>
+</a>
+</div>
+<!-- <div id="language-select">
+</div> -->
+</div>
+
+<div class="footer-left">
+<nav id="footer-nav">
+<ul>
+<li>
+<a href="/blog/">Blog</a>
+</li>
+<li>
+<a href="/about-webs">About Us</a>
+</li>
+<li>
+<a href="/careers">Careers</a>
+</li>
+<li>
+<a href="/terms-of-service">Terms</a>
+</li>
+<li>
+<a href="/help-and-contact">Help &amp; Contact</a>
+</li>
+<li>
+<a href="/report-abuse">Report a Site</a>
+</li>
+<li>
+<a href="/affiliates">Affiliates</a>
+</li>
+</ul>
+</nav>
+
+<p class="tips">Ready to get started? Get helpful tips for <a href="http://www.webs.com/small-business">small business websites</a> and more in our resource center. </p>
+
+<span class="copyright">
+��2014 iMath. All Rights Reserved
+</div>
+</div>
+</footer>
